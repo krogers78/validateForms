106c48bccbe1eac343788fca997f36cc54f924f5
Brett Rogers <bdrogers@fullsail.edu>
2017-08-20 21:17:33 -0400

    Most CSS and JavaScript complete

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ const zipcode = document.querySelector('#zipcode')
 const country = document.querySelector('#country')
 const continueForward = document.querySelector('#proceed')
 const shippingForm = document.querySelector('#shippingForm')
+const submit = document.querySelector('#submitform')
 // all the radio buttons
 const medium = document.querySelector('#shirtmedium')
 const large = document.querySelector('#shirtlarge')
@@ -18,6 +19,21 @@ const red = document.querySelector('#shirtred')
 const black = document.querySelector('#shirtblack')
 const blue = document.querySelector('#shirtblue')
 
+const checkMarkSVG = `<svg width="88px" height="88px" viewBox="0 0 88 88" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+            <!-- Generator: Sketch 42 (36781) - http://www.bohemiancoding.com/sketch -->
+            <title>circle-check</title>
+            <desc>Created with Sketch.</desc>
+            <defs></defs>
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="desktop-success" transform="translate(-415.000000, -415.000000)" fill="#4990E2">
+                    <g id="circle-check" transform="translate(415.000000, 415.000000)">
+                        <g id="Layer_1">
+                            <path d="M44,0 C19.7,0 0,19.699 0,44 C0,68.301 19.7,88 44,88 C68.3,88 88,68.301 88,44 C88,19.699 68.3,0 44,0 Z M44,80 C24.118,80 8,63.882 8,44 C8,24.118 24.118,8 44,8 C63.882,8 80,24.118 80,44 C80,63.882 63.882,80 44,80 Z M54.627,33.17 L40.484,47.312 L34.828,41.656 C33.266,40.094 30.734,40.094 29.171,41.656 C27.609,43.218 27.609,45.751 29.171,47.314 L37.655,55.795 C39.217,57.357 41.751,57.357 43.312,55.795 L60.283,38.825 C61.845,37.263 61.847,34.731 60.284,33.168 C58.723,31.609 56.189,31.609 54.627,33.17 Z" id="circle-check"></path>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>`
 
 class ValidateClass {
   constructor(input, type) {
@@ -138,7 +154,8 @@ continueForward.addEventListener('click', e => {
     if (noColor) {
       document.querySelector('#colorSelect').insertAdjacentHTML('afterEnd', '<p class="error">A color needs to be selected</p>')
     }
-  } else {
+  }
+  else {
     if (address2.value == '') {
       document.querySelector('#shippingAddress address').innerHTML = `<address>
                                                                           ${fname.value}</br>
@@ -147,7 +164,10 @@ continueForward.addEventListener('click', e => {
                                                                           ${country.value}
                                                                       </address>`
       CheckTheSizeAndColor()
-    } else {
+      submitform.disabled = false
+
+    }
+    else {
       document.querySelector('#shippingAddress address').innerHTML = `<address>
                                                                           ${fname.value}</br>
                                                                           ${address.value}<br>
@@ -156,8 +176,38 @@ continueForward.addEventListener('click', e => {
                                                                           ${country.value}
                                                                       </address>`
       CheckTheSizeAndColor()
+      submitform.disabled = false
     }
+
   }
+})
+submitform.addEventListener('click', e => {
+  document.querySelector('body').innerHTML = `<div class="orderComplete">
+                                                <h2>Order Completed</h2>
+                                                  <h3>Order Summary</h3>
+                                                    <div id="productName">
+                                                      <h4>Basic V-Neck Tee</h4>
+                                                      <p>$20.00</p>
+                                                    </div>
+                                                    <div id="shippingCost">
+                                                      <h4>Shipping</h4>
+                                                      <p>Free</p>
+                                                    </div>
+                                                    <div id="totalPrice">
+                                                      <h4>Total</h4>
+                                                      <p>$20.00</p>
+                                                    </div>
+                                                    <div id="shippingAddress">
+                                                      <h4>Shipping Address</h4>
+                                                      <address>
+                                                        ${fname.value}<br>
+                                                        ${address.value}<br>
+                                                        ${city.value}, ${state.value} ${zipcode.value}<br>
+                                                        ${country.value}
+                                                      </address>
+                                                    </div>
+                                                    ${checkMarkSVG}
+                                                  </div>`
 })
 
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Form Validation
+The intent of the project is to showcase some basic form validation. A user will be prompted with errors if the required inputs have not been filled out. Upon filling out the form the user will then be able to complete the order and be shown an order complete screen to display their success.
+#Live Site
+The website can be viewed [here](http://krogers78.github.io/validateForms)
